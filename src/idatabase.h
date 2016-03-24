7cc4da19521b827a1cac662969a0c161136f8243
Andreas Coroiu <andreas.coroiu@gmail.com>
2016-03-24 10:29:54 +0100

    Changed database pointers to shared_ptr

--- src/idatabase.h
+++ src/idatabase.h
@@ -2,6 +2,7 @@
 #define IDATABASE_H
 
 #include <vector>
+#include <memory>
 #include "inewsgroup.h"
 #include "iarticle.h"
 
@@ -12,13 +13,13 @@ public:
   // http://www.geeksforgeeks.org/pure-virtual-destructor-c/
   virtual ~IDatabase() {};
 
-  virtual std::vector<INewsgroup*> getNewsgroups();
-  virtual INewsgroup* getNewsgroup(const int& id);
-  virtual INewsgroup* addNewsgroup(const int& id, const std::string &title);
+  virtual std::vector<std::shared_ptr<INewsgroup>> getNewsgroups();
+  virtual std::shared_ptr<INewsgroup> getNewsgroup(const int& id);
+  virtual std::shared_ptr<INewsgroup> addNewsgroup(const int& id, const std::string &title);
 
-  virtual std::vector<IArticle*> getArticles(const int& newsgroup_id);
-  virtual IArticle* getArticle(const int& id);
-  virtual IArticle* addArticle(const int& id, const std::string &title, 
+  virtual std::vector<std::shared_ptr<IArticle>> getArticles(const int& newsgroup_id);
+  virtual std::shared_ptr<IArticle> getArticle(const int& id);
+  virtual std::shared_ptr<IArticle> addArticle(const int& id, const std::string &title, 
     const std::string &author, const std::String &text);
 
 protected:
