2f026c866716c770f629f79dd08f508c85f8ea1a
Andreas Coroiu <andreas.coroiu@gmail.com>
2016-03-28 18:28:19 +0200

    Fix/added some code to exceptions.

--- src/database_exceptions.h
+++ src/database_exceptions.h
@@ -5,6 +5,7 @@
 
 class obj_already_exists : public std::exception 
 {
+public:
 	virtual const char* what() const throw()
 	{
 		return "The object cannot be created because it already exists.";
@@ -13,14 +14,22 @@ class obj_already_exists : public std::exception
 
 class obj_not_found : public std::exception
 {
+public:
+	obj_not_found(const string &msg) : msg(msg) {};
 	virtual const char* what() const throw()
 	{
-		return "The object you are searching for could not be found.";
+		if (msg.empty())
+			return "The object you are searching for could not be found.";
+		else 
+			return msg;
 	}
+private:
+	string msg;
 };
 
 class func_not_supported : public std::exception
 {
+public:
 	virtual const char* what() const throw()
 	{
 		return "The function you are trying to use is not supported at the moment.";
@@ -29,6 +38,7 @@ class func_not_supported : public std::exception
 
 class func_not_implemented : public std::exception
 {
+public:
 	virtual const char* what() const throw()
 	{
 		return "The function you are trying to use is not yet implemented. Work in progress.";
