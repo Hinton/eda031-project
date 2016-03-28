8ddc3c0b5e5b434395156a3180d89df08222858c
Andreas Coroiu <andreas.coroiu@gmail.com>
2016-03-28 16:50:08 +0200

    Make functions pure virtual.

--- src/idatabase.h
+++ src/idatabase.h
@@ -12,23 +12,21 @@ public:
 	// http://www.geeksforgeeks.org/pure-virtual-destructor-c/
 	virtual ~IDatabase() { };
 
-	virtual std::vector<std::shared_ptr<INewsgroup>> get_newsgroups();
+	virtual std::vector<std::shared_ptr<INewsgroup>> get_newsgroups() = 0;
 
-	virtual std::shared_ptr<INewsgroup> get_newsgroup(const int &id);
+	virtual std::shared_ptr<INewsgroup> get_newsgroup(const int &id) = 0;
 
-	virtual std::shared_ptr<INewsgroup> add_newsgroup(const int &id, const std::string &title);
-
-	virtual bool remove_newsgroup(const int &id);
+	virtual std::shared_ptr<INewsgroup> add_newsgroup(const int &id, const std::string &title) = 0;
 
 	virtual bool remove_newsgroup(const int &id) = 0;
 
-	virtual std::vector<std::shared_ptr<IArticle>> get_articles(const int &newsgroup_id);
+	virtual std::vector<std::shared_ptr<IArticle>> get_articles(const int &newsgroup_id) = 0;
 
 	virtual std::shared_ptr<IArticle> get_article(const int &newsgroup_id, const int &article_id) = 0;
 
 	virtual std::shared_ptr<IArticle> add_article(const int &newsgroup_id, const int &article_id,
 												  const std::string &title,
-												  const std::string &author, const std::string &text);
+												  const std::string &author, const std::string &text) = 0;
 
 	virtual bool remove_article(const int &newsgroup_id, const int &article_id) = 0;
 
