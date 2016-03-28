862b40055609e663b3062ba2eaacccb349e47727
Andreas Coroiu <andreas.coroiu@gmail.com>
2016-03-28 14:36:37 +0200

    Fixed exceptions.

--- src/database_exceptions.h
+++ src/database_exceptions.h
@@ -3,20 +3,36 @@
 
 #include <exception>
 
-class obj_already_exists : public exception
+class obj_already_exists : public std::exception 
 {
-  virtual const char* what() const throw()
-  {
-    return "The object cannot be created because it already exists.";
-  }
+	virtual const char* what() const throw()
+	{
+		return "The object cannot be created because it already exists.";
+	}
 };
 
-class obj_not_found : public exception
+class obj_not_found : public std::exception
 {
-  virtual const char* what() const throw()
-  {
-    return "The object you are searching for could not be found.";
-  }
+	virtual const char* what() const throw()
+	{
+		return "The object you are searching for could not be found.";
+	}
+};
+
+class func_not_supported : public std::exception
+{
+	virtual const char* what() const throw()
+	{
+		return "The function you are trying to use is not supported at the moment.";
+	}
+};
+
+class func_not_implemented : public std::exception
+{
+	virtual const char* what() const throw()
+	{
+		return "The function you are trying to use is not yet implemented. Work in progress.";
+	}
 };
 
 #endif
